6f0a1fa42f06ee7eb63e5878aee77e9cc768ee3f
coderquill <coderquill@gmail.com>
2020-05-04 17:50:00 +0530

    introduction to node-js

---
+++ Backend/intro-to-node/index.js
@@ -0,0 +1,7 @@
+const superHero = require("superheroes");
+ var randomSuperHeroName = superHero.random();
+
+ const superVillain = require("supervillains");
+  var randomSuperVillainName = superVillain.random();
+
+console.log(randomSuperHeroName+" X "+randomSuperVillainName);
